cb60a347600b7f16f5784ecc1f99745b266dc43b
MS <rhfh56@naver.com>
2021-08-01 17:22:29 +0900

    스펠링 수정

---
+++ src/main/java/Practice.java
@@ -0,0 +1,16 @@
+public class practice {
+    public static void main(String[] args) {
+        System.out.println(7);
+        System.out.println(3.15);
+        System.out.println("3 + 5 = " + 8);
+        System.out.println(3.15 + "는 실수입니다.");
+        System.out.println("3 + 5" + " 의 연산 결과는 8입니다.");
+        System.out.println(3 + 5);
+        System.out.println(" 2 x 6 = " + 12);
+        System.out.println(" 6 + 6 = " + 12);
+        System.out.println(12);
+        System.out.println(" 8 + 4 = " + 12);
+        System.out.println(" 12 x 1 = " + 12);
+        /* 클레스 이름 스펠링 수정 */
+    }
+}
