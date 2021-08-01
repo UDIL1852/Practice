5a5c15d9cab976f9a7f888b2d827fc8a9a9905b8
MS <rhfh56@naver.com>
2021-08-01 23:32:28 +0900

    덧셈 뺄셈

---
+++ src/main/java/Practice2.java
@@ -0,0 +1,17 @@
+public class Practice2 {
+    public  static  void main(String[] args) {
+
+        int num1;   // 변수 num1의 선언
+        num1 = 10;  // 변수 num1에 10을 저장
+        int num2 = 20;  // 변수 num2 선언과 동시에 20으로 초기화
+        int num3 = num1 + num2;  // 두 변수 값을 대상으로 덧셈
+        System.out.println(num1 + " + " + num2 + " = " + num3);
+
+                double num4, num5;  // 두 개의 변수 동시 선언
+                double result;
+                num4 = 1.0000001;
+                num5 = 2.0000001;
+                result = num4 + num5;
+                System.out.println(result);
+    }
+}
