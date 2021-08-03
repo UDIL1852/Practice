f617c2e10215a1bfa47e8e4b663d56e357f70831
MS <rhfh56@naver.com>
2021-08-03 20:59:50 +0900

    계산식을 쓰고 답을 출력하는 법2

--- src/main/java/chapter4/Calculation.java
+++ src/main/java/chapter4/Calculation.java
@@ -20,6 +20,41 @@ public class Calculation {
         // 이 위에는 내가 한거 아래는 친구가 알려준 거
         int ans=((25*5)+(36-4)-72)/5;
         System.out.println(" The answer is = " + ans);
+        // 이어서 다른거
+        int ans2 = 11;
+        int ans3 = 22;
+        boolean result;
+
+        result = (1 < ans2) && (ans2 < 100);
+        System.out.println("1 초과 100 미만인가?" + result);
+
+        result = (ans3 % 2) == 0 || ((ans3 % 3) == 0);
+        System.out.println(" 2 또는 3의 배수인가? " + result);
+
+        result = !(ans2 != 0);
+        System.out.println(" 0 인가 ? " + result);
+        // 문제 변수 n1, n2, n3가 다음과 같을 때 n1 > n2 > n3 이면 true, 아니면 false를 출력하는 프로그램을 작성해보자
+        // n1 = ((25+5)+(36/4)-72)*5
+        // n2 = ((25*5)+(36-4)+71)/4
+        // n3 = (128/4)*2
+        int quiz = ((25+5)+(36/4)-72)*5;
+        System.out.println( " 퀴즈1 답은?  = " + quiz);
+        int quiz2 = ((25*5)+(36-4)+71)/4;
+        System.out.println(" 퀴즈2 답은? = " + quiz2);
+        int quiz3 = (128/4)*2;
+        System.out.println(" 퀴즈3 의 답은? = " + quiz3);
+
+        result = (quiz > quiz2);
+        System.out.println(" 퀴즈 1보다 퀴즈 2가 큼? = " + result);
+        result = (quiz2 < quiz3);
+        System.out.println(" 퀴즈 3이 퀴즈 2보다 큼?" + result);
+        //문제는 n2 > n3 를 물어봤지만 그냥 true 가 만들고 싶어서 바꿈
+
+
+
+
+
+
 
     }
 
