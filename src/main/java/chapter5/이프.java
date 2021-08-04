e0667be8e30cfbe49e56e81c63918c394a7cbd37
MS <rhfh56@naver.com>
2021-08-04 18:18:50 +0900

    문제풀때 창의성 좋아야하네...

--- src/main/java/chapter5/이프.java
+++ src/main/java/chapter5/이프.java
@@ -17,14 +17,37 @@ public class 이프 {
             else
                 System.out.println("n1 == n2 is false");
 
-        int num = 120;
+        int num = 110;
 
-        if (num < 0)
+        if (num < 0) {
             System.out.println(" 0 미만 ");
-        else if (num < 100)
+        }
+        else if (num < 100) {
             System.out.println(" 0이상 100미만");
-        else
+        }
+            else {
             System.out.println(" 100 이상 ");
+        }
+
+
+        //아래 코드에서는 두 개의 if를 사용중. 이를 하나의 if만 사용하도록 변경해보자
+        //int num1 = 120;
+        //if(num1 > 0) {
+            //if ((num % 2) == 0)
+                //System.out.println("양수이면서 짝수");
+        // }
+
+        //내 예상 답변
+        int num1 = 120;
+        //if(num1 > 0)
+         // ((num1 % 2) == 0)
+        //System.out.println("양수이면서 짝수");
+
+        //응 아니야 실제 답 이거야 ㅋ
+        if(num1 > 0 && (num1 % 2) == 0)
+            System.out.println("양수이면서 짝수");
+
+
 
 
         }
