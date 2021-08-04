f0add5fa706104a6aa2760a9ac56bae22e404491
MS <rhfh56@naver.com>
2021-08-04 17:37:58 +0900

    else 에러 짜증나네

---
+++ src/main/java/chapter5/이프.java
@@ -0,0 +1,29 @@
+package chapter5;
+
+public class 이프 {
+    public static void main(String[] args) {
+        int num = 120;
+        if (num < 0)
+            System.out.println(" 0 미만 ");
+        else if (num < 100) ;
+            System.out.println(" 0이상 100미만");
+        else
+            System.out.println(" 100 이상 ");
+
+
+        //int n1 = 5;
+        //int n2 = 7;
+
+        //if 문
+        //if(n1 < n2) {
+            //System.out.println("n1 < n2 is true"); //n1 < n2 면 실행되는 문장
+
+
+            //if ~ else 문
+            //if (n1 == n2)
+                //System.out.println("n1 == n2 is true");
+
+            //else
+                //System.out.println("n1 == n2 is false");
+        }
+}
